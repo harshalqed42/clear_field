d7a3f5fbdd116b9e145978825e95061f006ac3a7
hash6 <harshal.pradhan@qed42.com>
2020-01-24 10:53:07 +0530

    Commiting Clear Field.

--- js/clear-field.js
+++ js/clear-field.js
@@ -1,42 +1,53 @@
 (function ($, Drupal, drupalSettings) {
     'use strict';
     Drupal.behaviors.clear_field = {
-        attach: function (context, settings) {
-            var form_text_class = drupalSettings.clear_field.form_text_class;
-            var span_class = drupalSettings.clear_field.span_class;
-            $('input.'+form_text_class, context).once(form_text_class).each(function () {
-                if ($(this).val()) {
-                  var parent = $(this).parent();
-                  parent.append(drupalSettings.clear_field.close_button);
-                }
-            });
-            $(document).on('click', '.'+span_class, function(e) {
-                e.preventDefault();
-                var parent = $(this).parent().find('input');
-                parent.val('');
-                $(this).remove();
-            });
-            $(document).on('keyup', '.'+form_text_class, function(e) {
-                var input_element = $(this).parent().find('input');
-                var span_element = $(this).parent().find('span');
-                if (!input_element.val()){
-                    span_element.remove();
-                }
-            });
-            $('.'+ form_text_class).change(function(){
-                var span_element = $(this).parent().find('span');
-                if ($(this).val() && !span_element.hasClass(span_class)) {
-                    var parent = $(this).parent();
-                    parent.append(drupalSettings.clear_field.close_button);
-                }
-            });
+      attach: function (context, settings) {
+        var form_text_class = drupalSettings.clear_field.form_text_class;
+        var span_class = drupalSettings.clear_field.span_class;
+        var classes = '';
+          $.map( form_text_class, function(value, index ) {
+              if (form_text_class.length && (form_text_class.length - 1) == index) {
+                    classes = classes + 'input.' + value;
+              }
+              if (form_text_class.length && (form_text_class.length - 1) != index) {
+                    classes = classes + 'input.' + value + ', ';
+              }
+          });
+          $(classes, context).once(classes).each(function () {
+            if ($(this).val()) {
+                var parent = $(this).parent();
+                parent.append(drupalSettings.clear_field.close_button);
+              //clearRemove();
+            }
+          });
 
-
-        }
+          $(document).on('click', '.' + span_class, function(e) {
+            e.preventDefault();
+            var parent = $(this).parent().find('input');
+            parent.val('');
+            $(this).remove();
+          });
+          $(classes).change(function() {
+            var span_element = $(this).parent().find('span');
+            if ($(this).val() && !span_element.hasClass(span_class)) {
+              // clearRemove();
+                var parent = $(this).parent();
+                parent.append(drupalSettings.clear_field.close_button);
+            }
+          });
+          $(document).on('keyup', '.'+ classes, function(e) {
+            var input_element = $(this).parent().find('input');
+            var span_element = $(this).parent().find('span');
+            if (!input_element.val()){
+              span_element.remove();
+            }
+          });
+      }
     };
-    function clearRemove(span_class) {
-       // console.log(parent);
-       // parent.append('<span class="'+span_class+'" ><a class="close" href="#" >ENTERS</a></span>');
+
+    function clearRemove() {
+        var parent = $(this).parent();
+        parent.append(drupalSettings.clear_field.close_button);
     }
 
 })(jQuery, Drupal, drupalSettings);
